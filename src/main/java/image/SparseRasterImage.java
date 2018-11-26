9ec50e0b8d529b20629206cb517f9eb915f60d98
antho <anthony_pro1998@outlook.fr>
2018-11-26 18:38:05 +0100

    Ajout de deux méthodes, setPixelColor et getPixelColor.

--- src/main/java/image/SparseRasterImage.java
+++ src/main/java/image/SparseRasterImage.java
@@ -23,15 +23,36 @@ public class SparseRasterImage extends RasterImage {
         createRepresentation();
     }
 
+    @Override
     public void createRepresentation()
     {
         for(int i = 0; i <width;i++)
         {
             for(int j = 0 ; j<height ; j++)
             {
-                pixelsMap.put(new Point(i, j), super.pixels[i][j]);
+                if(super.pixels[i][j]!=Color.WHITE)
+                    pixelsMap.put(new Point(i, j), super.pixels[i][j]);
             }
         }
+    }
+
+    @Override
+    public void setPixelColor(Color color, int x, int y)
+    {
+        super.setPixelColor(color, x, y);
+        pixelsMap.clear();
+        createRepresentation();
+    }
+
+    @Override
+    public Color getPixelColor(int x, int y)
+    {
+
+        if(pixelsMap.containsKey(new Point(x, y)))
+            return pixelsMap.get(new Point(x, y));
+        else
+            return Color.WHITE;
+
     }
 
 
